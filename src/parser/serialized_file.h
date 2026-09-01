be95b75e8ec2e6871ea039f25169b0db86a0c516
wwwaboflah2000-tech <www.aboflah2000@gmail.com>
2026-09-01 18:57:18 +0300

    Initialize ObjectInfo members and update parse method

--- src/parser/serialized_file.h
+++ src/parser/serialized_file.h
@@ -8,14 +8,14 @@
 #include <cstdint>
 
 struct ObjectInfo {
-    int64_t path_id;
-    uint64_t byte_start;
-    uint32_t byte_size;
-    int32_t type_id;
-    int32_t class_id;
+    int64_t path_id = 0;
+    uint64_t byte_start = 0;
+    uint32_t byte_size = 0;
+    int32_t type_id = 0;
+    int32_t class_id = 0;
+    std::string source_file; // اسم الملف الأصلي القادم منه
 };
 
-// Unity Class IDs
 enum UnityClassID {
     ClassID_GameObject = 1,
     ClassID_Transform = 4,
@@ -24,11 +24,14 @@ enum UnityClassID {
     ClassID_Texture2D = 28,
     ClassID_MeshFilter = 33,
     ClassID_Mesh = 43,
+    ClassID_TextAsset = 49,
+    ClassID_AudioClip = 83,
     ClassID_SkinnedMeshRenderer = 137
 };
 
 class SerializedFile {
 public:
+    std::string file_name;
     uint32_t metadata_size = 0;
     uint64_t file_size = 0;
     uint32_t version = 0;
@@ -39,7 +42,7 @@ public:
     std::vector<ObjectInfo> objects;
     std::unordered_map<int64_t, size_t> path_id_to_index;
 
-    bool parse(const uint8_t* raw_data, size_t raw_size);
+    bool parse(const uint8_t* raw_data, size_t raw_size, const std::string& name = "");
 };
 
 #endif
