70c11d505d79df1ab158b17ecc831f644fdf9759
wwwaboflah2000-tech <www.aboflah2000@gmail.com>
2026-09-02 00:39:26 +0300

    Add new Godot classes to unity_studio.h

--- src/godot_bindings/unity_studio.h
+++ src/godot_bindings/unity_studio.h
@@ -3,6 +3,10 @@
 
 #include <godot_cpp/classes/ref_counted.hpp>
 #include <godot_cpp/classes/file_access.hpp>
+#include <godot_cpp/classes/image.hpp>
+#include <godot_cpp/classes/array_mesh.hpp>
+#include <godot_cpp/classes/audio_stream_wav.hpp>
+#include <godot_cpp/classes/node3d.hpp>
 #include <godot_cpp/variant/packed_string_array.hpp>
 #include <godot_cpp/variant/packed_byte_array.hpp>
 #include <godot_cpp/variant/dictionary.hpp>
@@ -10,6 +14,7 @@
 
 #include "unity_fs.h"
 #include "serialized_file.h"
+#include "unity_objects.h"
 
 namespace godot {
 
@@ -19,7 +24,10 @@ class UnityStudio : public RefCounted {
 private:
     UnityFSArchive current_archive;
     std::vector<SerializedFile> loaded_files;
+    PackedByteArray raw_master_buffer;
     bool is_loaded = false;
+
+    const uint8_t* get_object_data(int64_t path_id, uint32_t &out_size, int &out_version);
 
 protected:
     static void _bind_methods();
@@ -32,9 +40,14 @@ public:
     bool load_raw_data(const PackedByteArray &pba, const String &name);
     void clear_all();
 
-    PackedStringArray get_internal_files();
     int64_t get_object_count();
     Array get_game_objects_list();
+
+    // ⚡ دوال الفك والمعاينة الخارقة ⚡
+    String get_text_asset(int64_t path_id);
+    Ref<Image> get_texture_image(int64_t path_id);
+    Ref<AudioStreamWAV> get_audio_stream(int64_t path_id);
+    Ref<ArrayMesh> get_mesh(int64_t path_id);
 };
 
 } // namespace godot
