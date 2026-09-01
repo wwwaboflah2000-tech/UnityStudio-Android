6c00719e1c623e8313e33977803d9e95d7734f49
wwwaboflah2000-tech <www.aboflah2000@gmail.com>
2026-09-01 16:56:23 +0300

    Add header file for Unity Studio module

---
+++ src/register_types.h
@@ -0,0 +1,9 @@
+#ifndef REGISTER_TYPES_H
+#define REGISTER_TYPES_H
+
+#include <godot_cpp/core/class_db.hpp>
+
+void initialize_unity_studio_module(godot::ModuleInitializationLevel p_level);
+void uninitialize_unity_studio_module(godot::ModuleInitializationLevel p_level);
+
+#endif
