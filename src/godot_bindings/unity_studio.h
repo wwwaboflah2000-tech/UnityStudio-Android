4171575a1269632791a19803cb083566f7ff555b
wwwaboflah2000-tech <www.aboflah2000@gmail.com>
2026-09-01 21:05:17 +0300

    Add load_raw_data and clear_all methods
    Added methods to handle raw data and clear files.

--- src/godot_bindings/unity_studio.h
+++ src/godot_bindings/unity_studio.h
@@ -4,6 +4,7 @@
 #include <godot_cpp/classes/ref_counted.hpp>
 #include <godot_cpp/classes/file_access.hpp>
 #include <godot_cpp/variant/packed_string_array.hpp>
+#include <godot_cpp/variant/packed_byte_array.hpp>
 #include <godot_cpp/variant/dictionary.hpp>
 #include <godot_cpp/variant/array.hpp>
 
@@ -17,7 +18,7 @@ class UnityStudio : public RefCounted {
 
 private:
     UnityFSArchive current_archive;
-    std::vector<SerializedFile> loaded_files; // جميع الملفات المفككة
+    std::vector<SerializedFile> loaded_files;
     bool is_loaded = false;
 
 protected:
@@ -28,6 +29,9 @@ public:
     ~UnityStudio();
 
     bool load_bundle(const String &file_path);
+    bool load_raw_data(const PackedByteArray &pba, const String &name);
+    void clear_all();
+
     PackedStringArray get_internal_files();
     int64_t get_object_count();
     Array get_game_objects_list();
